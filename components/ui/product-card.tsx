f2059cc560be91a64ff57be3c5101f85867ba64d
reannu123 <reannumon123@gmail.com>
2024-03-10 21:38:50 +0800

    Add ModalProvider and usePreviewModal hook

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,19 +2,25 @@
 
 import { Product } from "@/types";
 import Image from "next/image";
-import React from "react";
+import React, { MouseEventHandler } from "react";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { usePreviewModal } from "@/hooks/use-preview-modal";
 
 interface ProductCard {
   data: Product;
 }
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+    previewModal.onOpen(data);
   };
   return (
     <div
@@ -31,7 +37,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full bottom-5 px-6">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={
                 <Expand
                   size={20}
