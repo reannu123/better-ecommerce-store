27af1025ebf4f2440bd9e8928fa7a46c95e190b9
reannu123 <reannumon123@gmail.com>
2024-03-12 05:02:10 +0800

    Update Dockerfile and start.sh

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,6 @@ RUN npm i
 COPY . .
 ENV PORT=3000
 
-
-RUN npm run build
 EXPOSE 3000
 # Run npm start to start the server
 RUN chmod +x start.sh
--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# Add your commands here
-# npm run generate
-# echo "Preparing to start the server..."
-
-# Then start the server
+npm run build
 npm start
