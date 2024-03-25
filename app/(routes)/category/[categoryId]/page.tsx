0237ed4c63c8908bd54ea8d95dbdbb94edb26e7e
reannu123 <reannumon123@gmail.com>
2024-03-25 18:31:44 +0800

    Remove unused color and size actions

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,7 +1,5 @@
 import getCategory from "@/actions/get-category";
-import getColors from "@/actions/get-colors";
 import getProducts from "@/actions/get-products";
-import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 import React from "react";
