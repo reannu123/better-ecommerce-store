18ec9030053cede89f217f128147294c3adb677d
reannu123 <reannumon123@gmail.com>
2024-04-09 01:44:05 +0800

    Update API URLs in get-billboard, get-categories, get-category, get-product, and get-products actions

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/types";
+import { env } from "next-runtime-env";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+const URL = `${env("NEXT_PUBLIC_API_URL")}/products`;
 
 const getProduct = async (id: string): Promise<Product> => {
   const res = await fetch(`${URL}/${id}`);
